0232e090a90eca3f515bb2259ae98baa2153c1bb
Grzegorz Wojtanowicz <g.wojtanowicz@zoovu.com>
2022-08-03 15:11:27 +0200

    Added mui provider and baseline

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+const theme = createTheme();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
